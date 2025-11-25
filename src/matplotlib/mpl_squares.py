ba7b1acfd6adf2ad95e068b3ddcb9ad87bb4a3e4
Vitaliy Yakovlev <vintyak@gmail.com>
2025-11-25 23:48:50 +0300

    Add simple diagram, fix run

---
+++ src/matplotlib/mpl_squares.py
@@ -0,0 +1,17 @@
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
+
+squares = [1, 4, 9, 16, 25]
+fig, ax = plt.subplots()
+ax.plot(squares, linewidth=3)
+
+# Задание заголовка диаграммы и меток осей.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Задание размера шрифта делений на осях.
+ax.tick_params(labelsize=14)
+
+plt.show()
