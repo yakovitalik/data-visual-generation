838855df8cee1d2356c67209e12abd49c75cfb95
Vitaliy Yakovlev <vintyak@gmail.com>
2025-12-26 16:01:19 +0300

    Add date to diagram

--- src/download_data/sitka_highs.py
+++ src/download_data/sitka_highs.py
@@ -1,5 +1,9 @@
 from pathlib import Path
+from datetime import datetime
 import csv
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
 
 path = Path('weather_data/sitka_weather_07-2021_simple.csv')
 lines = path.read_text().splitlines()
@@ -7,5 +11,24 @@ lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-for index, column_header in enumerate(header_row):
-    print(index, column_header)
+# Извлечение дат и максимальных температур
+dates, highs = [], []
+for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    high = int(row[4])
+    dates.append(current_date)
+    highs.append(high)
+
+# Нанесение данных на диаграмму
+plt.style.use('classic')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, color='red')
+
+# Форматирование диаграммы
+ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
