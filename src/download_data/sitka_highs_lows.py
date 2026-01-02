63988613950dbae54f6624b97001e9aa10e8a396
Vitaliy Yakovlev <vintyak@gmail.com>
2026-01-02 21:03:25 +0300

    Add low temperature to diagram

--- src/download_data/sitka_highs_lows.py
+++ src/download_data/sitka_highs_lows.py
@@ -11,21 +11,24 @@ lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-# Извлечение дат и максимальных температур
-dates, highs = [], []
+# Извлечение дат, минимальных и максимальных температур
+dates, highs, lows = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    low = int(row[5])
     dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
-# Нанесение данных на диаграмму
+# Создание диаграммы высоких и низких температур из файла
 plt.style.use('classic')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, color='red')
+ax.plot(dates, lows, color='blue')
 
 # Форматирование диаграммы
-ax.set_title("Daily High Temperatures, 2021", fontsize=24)
+ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
